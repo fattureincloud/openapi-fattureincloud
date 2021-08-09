70295dd833dd2071ac335c9762c591b09c2dc278
Mauro Valota <maurovalota@fattureincloud.it>
2021-08-09 17:58:37 +0200

    Retrieves OAS KEY using python.

--- .github/workflows/scripts/retrieve_spec_id.py
+++ .github/workflows/scripts/retrieve_spec_id.py
@@ -3,45 +3,52 @@ import requests
 import base64
 import os
 
+
 base_url = 'https://dash.readme.com/api/v1/'
 version_url = '{}/version'.format(base_url)
 api_key = 'v8f6zlqQ6wIUxDzULdsHoTZrBXTeLUcK'
-curr_version = None
-
-# with open('../../../openapi.yaml', 'r') as stream:
-#     data_loaded = yaml.safe_load(stream)
-#     version = data_loaded['info']['versions']
-
-
 
 
 def main():
-    curr_version = '2.0.0'
+    curr_version = retrieve_version_from_openapi()
+    print('Required version: {}'.format(curr_version))
 
-    versions_list =  get_versions()
+    if curr_version:
+        print('Retrieving versions...')
+        versions_list =  get_versions()
+        print('Done.')
 
-    spec_id = None
-    last_version = None
+        spec_id = None
+        last_version = None
 
-    for elem in versions_list.json():
-        v = elem['version']
-        if v == curr_version:
-            spec_id = elem['_id']
-        if not last_version or last_version < v:
-            last_version = v
-    
-    print('Spec ID: {}').format(spec_id)
-    print('Last version: {}').format(last_version)
-            
+        for elem in versions_list.json():
+            v = elem['version']
+            if v == curr_version:
+                spec_id = elem['_id']
+            if not last_version or last_version < v:
+                last_version = v
         
-    if not spec_id:
-        res = create_version(curr_version, last_version)
-        spec_id = res['_id']
-    
-    os.environ['SPEC_ID'] = spec_id
+        if not spec_id:
+            print('Required version not found. Creating...')
+            print('Last version: {}'.format(last_version))
+            res = create_version(curr_version, last_version).json()
+            print(res)
+            print('Created.')
+            spec_id = res['_id']
+
+        print('Spec ID: {}'.format(spec_id))
+        os.environ['OAS_KEY'] = '{}:{}'.format(api_key, spec_id)
+    else:
+        raise Exception('Version not found!!!')
 
 
-    
+def retrieve_version_from_openapi():
+    version = None
+    with open('./openapi.yaml', 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+        version = data_loaded['info']['version']
+        version = version.replace('v', '')
+    return version
 
 
 def get_versions():
@@ -53,8 +60,8 @@ def get_versions():
     }
 
     response = requests.request('GET', version_url, headers=headers)
-
     return response
+
 
 def create_version(version, last_version):
     api_key_b64 = base64.b64encode(api_key.encode('utf-8')).decode('utf-8')
@@ -71,11 +78,10 @@ def create_version(version, last_version):
         'is_stable': False,
         'is_hidden': True
     }
+    print(payload)
 
-    response = requests.request('POST', version_url, headers=headers, data=payload)
-
+    response = requests.request('POST', version_url, headers=headers, json=payload)
     return response
-
 
 
 main()
