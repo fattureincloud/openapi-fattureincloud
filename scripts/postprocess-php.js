1e98b10b225c254a608f85185f97f92578f6cf12
Mauro Valota <maurovalota@fattureincloud.it>
2021-10-14 13:49:27 +0200

    ci: automates backporting and license management

--- scripts/postprocess-php.js
+++ scripts/postprocess-php.js
@@ -1,4 +1,5 @@
 var fs = require("fs")
+var jsonata = require("jsonata")
 
 var command = process.argv[2]
 
@@ -10,6 +11,18 @@ switch (command) {
     var version = process.argv[3]
     var path = process.argv[4]
     replaceVersionPlaceholder(version, path)
+    break
+  case 'backport':
+    var path = process.argv[3]
+    transformJsonByLocation(path, {'require': {'php': '>=7.1', 'guzzlehttp/guzzle': '>=6.2 <7.0.0'}, 'require-dev': {'phpunit/phpunit': '>=7.0 <8.0.0'}})
+    break
+  case 'license':
+    var license = process.argv[3]
+    var path = process.argv[4]
+    transformJsonByLocation(path, {'$': {'license': license}})
+    break
+  default:
+    console.err("Unrecognised command: " + command)
 }
 
 function cleanPHPComments(dir) {
@@ -58,5 +71,32 @@ function replaceVersionPlaceholder(version, path) {
         return console.log(err)
       }
     })
+  })
+}
+
+function transformJsonByLocation(path, updateMap) {
+  var data = fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err)
+    } else {
+      var json = JSON.parse(data)
+      Object.keys(updateMap).map(function(key, _) {
+        var update = updateMap[key]
+        var location = null
+        if (key == "$") {
+          location = key
+        } else {
+          location = `$.\"${key}\"`
+        }
+        var expression = jsonata(`$ ~> |${location}|${JSON.stringify(update)}|`)
+        json = expression.evaluate(json)
+      })
+
+      fs.writeFile(path, JSON.stringify(json, null, 4), "utf8", function (err) {
+        if (err) {
+          return console.log(err)
+        }
+      })
+    }
   })
 }
