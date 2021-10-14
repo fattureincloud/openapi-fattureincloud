8f731f14c8e9de03ffaa38a78be2f32052007df3
Valmoz <Valmoz@users.noreply.github.com>
2021-10-14 14:34:13 +0200

    ci: improved post processing

--- scripts/postprocess-php.js
+++ scripts/postprocess-php.js
@@ -1,4 +1,5 @@
 var fs = require("fs")
+var jsonata = require("jsonata")
 
 var command = process.argv[2]
 
@@ -6,10 +7,17 @@ switch (command) {
   case 'clean':
     process.argv.slice(3).forEach((val, _) => cleanPHPComments(val))
     break
-  case 'version':
-    var version = process.argv[3]
+  case 'backport':
+    var path = process.argv[3]
+    transformJsonByLocation(path, {'require': {'php': '>=7.1', 'guzzlehttp/guzzle': '>=6.2 <7.0.0'}, 'require-dev': {'phpunit/phpunit': '>=7.0 <8.0.0'}})
+    break
+  case 'license':
+    var license = process.argv[3]
     var path = process.argv[4]
-    replaceVersionPlaceholder(version, path)
+    transformJsonByLocation(path, {'$': {'license': license}})
+    break
+  default:
+    console.err("Unrecognised command: " + command)
 }
 
 function cleanPHPComments(dir) {
@@ -44,19 +52,29 @@ function cleanPHPComments(dir) {
   })
 }
 
-function replaceVersionPlaceholder(version, path) {
-  fs.readFile(path, "utf8", function (err, data) {
+function transformJsonByLocation(path, updateMap) {
+  var data = fs.readFile(path, "utf8", function (err, data) {
     if (err) {
       return console.log(err)
+    } else {
+      var json = JSON.parse(data)
+      Object.keys(updateMap).map(function(key, _) {
+        var update = updateMap[key]
+        var location = null
+        if (key == "$") {
+          location = key
+        } else {
+          location = `$.\"${key}\"`
+        }
+        var expression = jsonata(`$ ~> |${location}|${JSON.stringify(update)}|`)
+        json = expression.evaluate(json)
+      })
+
+      fs.writeFile(path, JSON.stringify(json, null, 4), "utf8", function (err) {
+        if (err) {
+          return console.log(err)
+        }
+      })
     }
-    var result = data.replace(
-      '@@@VERSION_PLACEHOLDER@@@',
-      version
-    )
-    fs.writeFile(path, result, "utf8", function (err) {
-      if (err) {
-        return console.log(err)
-      }
-    })
   })
 }
