9edeca2fde5a8b4326e7c4d87cbedcfae4d57766
Mauro Valota <Valmoz@users.noreply.github.com>
2022-01-31 09:55:37 +0100

    fix: added  fields to issued document and client models

--- scripts/format-yaml.js
+++ scripts/format-yaml.js
@@ -1,11 +1,16 @@
-const yaml = require('js-yaml');
-const fs   = require('fs');
+const yaml = require('js-yaml')
+const fs   = require('fs')
+
+var filePath = process.argv[2]
+
+console.log("Trying to format: " + filePath)
 
 // Get document, or throw exception on error
 try {
-  const doc = yaml.load(fs.readFileSync('./openapi.yaml', 'utf8'));
-  fs.writeFileSync('./openapi.yaml', yaml.dump(doc));
+  const doc = yaml.load(fs.readFileSync(filePath, 'utf8'))
+  fs.writeFileSync(filePath, yaml.dump(doc))
+  console.log("Done")
 } catch (e) {
-  console.log(e);
+  console.log(e)
 }
 
