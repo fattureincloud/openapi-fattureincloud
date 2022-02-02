881349706bf38eca0e22c894c5a51edaeb651def
Mauro Valota <Valmoz@users.noreply.github.com>
2022-02-02 15:44:50 +0100

    fix: added einvoice rejection reason endpoint

--- scripts/format-yaml.js
+++ scripts/format-yaml.js
@@ -1,11 +1,43 @@
-const yaml = require('js-yaml');
-const fs   = require('fs');
+const yaml = require('js-yaml')
+const fs   = require('fs')
+const glob = require("glob")
 
-// Get document, or throw exception on error
-try {
-  const doc = yaml.load(fs.readFileSync('./openapi.yaml', 'utf8'));
-  fs.writeFileSync('./openapi.yaml', yaml.dump(doc));
-} catch (e) {
-  console.log(e);
+var command = process.argv[2]
+
+var filePath = process.argv[3]
+
+switch (command) {
+  case 'single':
+    formatFile(filePath)
+
+    break
+
+  case 'all':
+    var options = {}
+    glob("**/*.yaml", options, function (er, files) {
+      // files is an array of filenames.
+      // If the `nonull` option is set, and nothing
+      // was found, then files is ["**/*.yaml"]
+      // er is an error object or null.
+      files.forEach(function(filePath){
+        formatFile(filePath)
+      });
+    })
+    break
+  default:
+    console.err("Unrecognised command: " + command)
 }
 
+
+function formatFile(filePath) {
+  console.log("Trying to format: " + filePath)
+
+  // Get document, or throw exception on error
+  try {
+    const doc = yaml.load(fs.readFileSync(filePath, 'utf8'))
+    fs.writeFileSync(filePath, yaml.dump(doc))
+    console.log("Done")
+  } catch (e) {
+    console.log(e)
+  }
+}
