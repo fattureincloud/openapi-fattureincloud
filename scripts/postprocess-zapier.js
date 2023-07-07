52c4eac559c05ef8efc45fefec71af431a1a2a50
Emanuele Saccomandi <emanuele.saccomandi@gmail.com>
2023-07-07 09:16:59 +0200

    ci: zapier postprocess only adds triggers

--- scripts/postprocess-zapier.js
+++ scripts/postprocess-zapier.js
@@ -17,47 +17,16 @@ function postProcessZapierFolder(dir)
           return console.log(err)
         }
         
-        // change extension
-        let filename = file.replace('.md', '.js')
-        if(file == 'README.md') filename = 'operations/actions.js'
-
-        // remove duplicate imports 
-        let result = data.replace(
-          /(const [^;]+;\n)(?=(.*\n)*\1)/g,
-          ''
-        )
-  
-        // unescape field names
-        result = result.replaceAll(
-          '\\_',
-          '_'
-        )
-
         if (path.basename(dir) == 'apis') {
           // add triggers
-          result = addTriggers(result)
-        }
-        
-        if (path.basename(dir) == 'samples') {
-          // fix samples
-          filename = filename.replace('.js', '.json')
-          result = fixSamples(result)
+          data = addTriggers(data)
         }
   
-        fs.writeFile(dir + filename, result, 'utf8', function (err) {
+        fs.writeFile(dir + file, data, 'utf8', function (err) {
           if (err) {
             return console.log(err)
           }
         })
-
-        if(file != filename){
-          fs.unlink(dir + file, (err) => {
-            if (err) {
-              console.error(err)
-              return
-            }
-          })
-        }
       })
     }
   })
@@ -77,13 +46,4 @@ function addTriggers(file)
     )
   });
   return file
-}
-
-function fixSamples(file)
-{
-  file = file.substring(0, file.lastIndexOf(',')) + '}'
-  let sample = JSON.parse(file);
-  let fixedSample = Object.entries(sample).reduce((tempSample, [key, value]) => (tempSample[key] = value?.['content']?.['application/json']?.['examples']?.['example-1']?.['value'], tempSample), {})
-  
-  return file = JSON.stringify(fixedSample, null, 4)
 }
